9dde9c57a5154a2b3b312149591a69e5317663a4
Michal Janaszek <mjanasze@intercars.eu>
2017-11-19 22:53:45 +0100

    stage3 add timeout to html route

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'cache-token';
 const urlsToCache = ['/', '/style.css', '/script.js', '/logo.png'];
 
-self.addEventListener('install', event =>
+self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
     )
-);
+});
 
 self.addEventListener('fetch', event => {
     event.respondWith(
